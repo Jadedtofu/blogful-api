3cfb76141a890ab84b434070b250ce3ab4afe8f2
Xiaolei Wang <xwang@sandiego.edu>
2019-10-08 21:42:21 -0700

    modified postgrator-config to match database_url for heroku

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,15 +1,12 @@
 require('dotenv').config();
 
 module.exports = {
-    "migrationDirectory": "migrations", // refers to folder in our app that contains the migration steps
-    "driver": "pg",                     // refers to same driver setting used to create Knex instance
+    "migrationDirectory": "migrations", 
+    // refers to folder in our app that contains the migration steps
+    "driver": "pg",                     
+    // refers to same driver setting used to create Knex instance
     "connectionString": (process.env.NODE_ENV === 'test')
         ? process.env.TEST_DATABASE_URL
-        : process.env.DATABSE_URL,
+        : process.env.DATABASE_URL,
     "ssl": !!process.env.SSL,
-    "host": process.env.MIGRATION_DATABASE_HOST,
-    "port": process.env.MIGRATION_DATABASE_PORT,
-    "database": process.env.MIGRATION_DATABASE_NAME,
-    "username": process.env.MIGRATION_DATABASE_USER,
-    "password": process.env.MIGRATION_DATABASE_PASS
 }
